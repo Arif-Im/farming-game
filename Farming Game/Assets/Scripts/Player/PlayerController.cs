3368ce758fb3ce5f9ff777440ad3240173116bf6
Arif Imran <i-arif@okakichi.co.jp>
2025-12-03 18:02:44 +0800

    Added Anims

--- Farming Game/Assets/Scripts/Player/PlayerController.cs
+++ Farming Game/Assets/Scripts/Player/PlayerController.cs
@@ -1,22 +1,26 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D _rb;
+    [FormerlySerializedAs("_rb")] 
+    [SerializeField] private Rigidbody2D playerRigidbody;
+    [SerializeField] private float xMaxSpeed = 5.0f;
+    [SerializeField] private float yMaxSpeed = 5.0f;
     
     private float _xSpeed;
     private float _ySpeed;
     
     private void Update()
     {
-        _rb.linearVelocity = new Vector2(_xSpeed, _ySpeed);
+        playerRigidbody.linearVelocity = new Vector2(_xSpeed, _ySpeed);
     }
 
     public void OnMove(InputValue inputValue)
     {
-        _xSpeed = inputValue.Get<Vector2>().x;
-        _ySpeed = inputValue.Get<Vector2>().y;
+        _xSpeed = inputValue.Get<Vector2>().x * xMaxSpeed;
+        _ySpeed = inputValue.Get<Vector2>().y * yMaxSpeed;
     }
 }
