84fe64e902fbfd63ff42f01800a6a707d3b2e2a7
Arif <i-arif@okakichi.co.jp>
2025-12-09 07:08:19 +0800

    progress

--- tale-of-gatto/Scenes/player.gd
+++ tale-of-gatto/Scenes/player.gd
@@ -1,26 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var terrain_manager = $"../Terrain Manager"
+@onready var player: CharacterBody2D = $"."
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var x_direction := Input.get_axis("move_left", "move_right")
 	var y_direction := Input.get_axis("move_up", "move_down")
 	
+	handle_movement(x_direction, y_direction)
+	handle_animation(x_direction, y_direction)
+	
+	if(Input.is_action_just_pressed("interact")):
+		terrain_manager._interact(player.position)
+
+func handle_movement(x_direction, y_direction):
 	if x_direction:
 		velocity.x = x_direction * SPEED
 	else:
@@ -30,9 +29,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = y_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	handle_animation(x_direction, y_direction)
-		
+
 func handle_animation(x_direction, y_direction):
 	if x_direction == 0 && y_direction == 0:
 		if animated_sprite.animation == "Walk Up":
